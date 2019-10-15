0a214635c63bed88f91f34a8d82b13dcc3d39401
Utkarsh Sinha <43957190+utkarsh-1106@users.noreply.github.com>
2019-10-15 19:54:01 +0530

    Update linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -5,20 +5,20 @@ class node {
 	}
 }
 class linkedList {
-	constructor(value) {
-		this.head = {
-			value: value,
-			next: null
+		constructor(value) {
+			this.head = {
+				value: value,
+				next: null
+			}
+			this.tail = this.head
+			this.length = 1
 		}
-		this.tail = this.head
-		this.length = 1
-	}
-	append(value) {
-		const newNode = new node(value)
-		this.tail.next = newNode
-		this.tail = newNode
-		this.length++
-	}
+		append(value) {
+			const newNode = new node(value)
+			this.tail.next = newNode
+			this.tail = newNode
+			this.length++
+		}
 		prepend(value) {
 			const newNode = new node(value)
 			newNode.next = this.head
